263e2283c199867b2b9a2545e0c148faaa032538
Danieltbz <68570543+Da17iel@users.noreply.github.com>
2022-09-27 18:43:11 +0200

    Updated gitignore file

--- auth/config.php
+++
@@ -1,8 +0,0 @@
-<?php
-session_start();
-$host = "localhost"; /* Host name */$user = "root"; /* User */$password = ""; /* Password */$dbname = "epiz_32582366_arduinoschulprojekt"; /* Database name */
-$con = mysqli_connect($host, $user, $password,$dbname);
-// Check connection
-if (!$con) {
-  die("Connection failed: " . mysqli_connect_error());
-}
